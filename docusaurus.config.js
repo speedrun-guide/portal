d7c1a52b0a890b9efc628fc3d6e62dcb0fc9cc28
ChrisUMB <kchrisumb@gmail.com>
2023-05-10 01:14:52 -0700

    Started working on documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Speedrun Guide',
+        title: 'Portal Speedrun Guide',
         logo: {
           alt: 'Speedrun Guide Logo',
           // src: 'img/logo.svg',
