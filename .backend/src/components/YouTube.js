a167497267683ae83bed608a18399b949a7e7c36
ChrisUMB <kchrisumb@gmail.com>
2023-05-12 16:38:36 -0700

    Modified youtube JS react to have a "dropdown" feature, and it doesn't work right.

--- .backend/src/components/YouTube.js
+++ .backend/src/components/YouTube.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-export default function YouTube({id, url, width, timestamp}) {
-    if(id == null && url == null) {
+export default function YouTube({id, url, width, timestamp, dropdown = false}) {
+    if (id == null && url == null) {
         console.error("YouTube component requires an ID or a URL, but neither was provided.")
         return null
     }
 
-    if(url == null) {
+    if (url == null) {
         url = "https://www.youtube.com/embed/" + id
-    } else if(!url.includes("/embed/")) {
+    } else if (!url.includes("/embed/")) {
         const match = url.match(/(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
 
-        if(match == null) {
+        if (match == null) {
             console.error("YouTube component requires a valid YouTube URL, but the provided URL was invalid.")
             return null
         }
@@ -20,13 +20,24 @@ export default function YouTube({id, url, width, timestamp}) {
         url = "https://www.youtube.com/embed/" + id
     }
 
-    if(timestamp != null) {
+    if (timestamp != null) {
         url = url + "?start=" + timestamp;
     }
 
-    return ( <div style={{display:"flex", justifyContent:"center"}}><iframe style={{width: width != null ? width : "66%", aspectRatio: "16 / 9"}} src={url}
-            title="YouTube video player" frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen></iframe></div>)
 
+    let tag = (<div style={{display: "flex", justifyContent: "center"}}>
+        <iframe style={{width: width != null ? width : "66%", aspectRatio: "16 / 9"}} src={url}
+                title="YouTube video player" frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen></iframe>
+    </div>);
+
+    if(dropdown === true) {
+        tag = (<details>
+            <summary>Video</summary>
+            {tag}
+        </details>)
+    }
+
+    return tag
 }
